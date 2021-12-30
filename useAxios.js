1cf83613d01c865b4b1706c356d3ba8845974b0f
Erin Kahn Smith <erinnicolekahn@gmail.com>
2021-12-30 15:53:44 -0500

    Update useAxios.js

--- useAxios.js
+++ useAxios.js
@@ -1,3 +1,5 @@
+// --- hook:
+
 import {useState, useEffect} from 'react';
 import axios from 'axios';
 
@@ -21,3 +23,24 @@ export default function useAxios(url) {
         error
     }
 }
+
+
+
+// --- Consumer Component:
+
+// import React, {useState, useEffect} from 'react';
+// import useAxios from "../hooks/useAxios";
+// const API_KEY = '';
+
+// export default function ComponentName(url) {
+//     const url = `https://api.openweathermap.org/data/2.5/onecall?&units=imperial&lat=${position.lat}&lon=${position.long}&exclude=daily&appid=${API_KEY}`;
+//     const {data, loading, error} = useAxios(url);
+//     if (loading || !data) return '...';
+//     if (error) return '';
+    
+//     return (<>
+//         <div className="data-container">
+//             {data.current.temp}
+//         </div>
+//     </>)
+// }
