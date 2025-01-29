64e4fe6d1276a1fa2d68168ecb8a6c5c5f4d3caf
Erin Kahn <erinnicolekahn@gmail.com>
2025-01-29 07:48:12 -0700

    Update useLocalStorage.js

--- useLocalStorage.js
+++ useLocalStorage.js
@@ -3,32 +3,54 @@
 import React, { useState, useEffect } from 'react';
 
 export const useLocalStorage = (key, defaultValue = '') => {
-    const [state, setState] = useState(
-        () => window.localStorage.getItem('key') || defaultValue,
-    )
+    const [storedValue, setStoredValue] = useState(() => {
+        try {
+           const item = window.localStorage.getItem('key') || defaultValue,
+           return item ? JSON.parse(item) : defaultValue;
+        } catch(error) {
+            console.error(error);
+            return defaultValue;
+        }
+    });
 
-    useEffect(() => {
-        window.localStorage.setItem('key', state)
-    }, [state])
+    const setValue = (value: T) => {
+        try {
+          setStoredValue(value);
+          window.localStorage.setItem(key, JSON.stringify(value));
+        } catch (error) {
+          console.error(error);
+        }
+    };
 
-    return [state, setState]
+    return [storedValue, setStoredValue] as const;
 }
 
 
-// ----- consumer component:
+// usage 1
 
-// export default function ComponentName() {
-//     const [name, setName] = useLocalStorage('name')
+const [theme, setTheme] = useLocalStorage('theme', 'light');
 
-//     const handleChange = event => setName(event.target.value);
+return (
+  <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
+    Toggle Theme
+  </button>
+);
 
-//     return (
-//         <div>
-//             <form>
-//                 <label htmlFor="name">Name: </label>
-//                 <input type="text" value={name} onChange={handleChange} id="name" />
-//             </form>
-//             {name ? <strong>Hi {name}</strong> : 'Please type your name'}
-//         </div>
-//     )
-// }
+
+// usage 2
+
+export default function ComponentName() {
+    const [name, setName] = useLocalStorage('name')
+
+    const handleChange = event => setName(event.target.value);
+
+    return (
+        <div>
+            <form>
+                <label htmlFor="name">Name: </label>
+                <input type="text" value={name} onChange={handleChange} id="name" />
+            </form>
+            {name ? <strong>Hi {name}</strong> : 'Please type your name'}
+        </div>
+    )
+}
